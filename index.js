f0937f2951364cafb2cfa2f26077dcc1a893cdf9
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-06-29 11:30:49 +0300

    Adds variables with arithmetic operators in index.js

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 10;
+let num4 = 6;
+let multiply = (num1 * num2);
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = num3%num4;
+
+let max = Math.max(20, 11, 5, 9, 18);
+console.log(max);
+//multiply === 62;
+
+
+/*
+function multiply(num1, num2) {
+    let results = num1 * num2;
+    if (results === 62) {
+        return results;
+    } else {
+        return console.log(`Reenter numbers that will equate to 62`);
+    }
+}
+
+console.log(multiply(1, 62));
+*/
